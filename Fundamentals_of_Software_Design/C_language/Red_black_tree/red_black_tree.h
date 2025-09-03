fe4deb75a8f119b60a710a42c8ad2047230e36a4
Klimov Bogdan <136115919+KeroDeep@users.noreply.github.com>
2025-09-03 10:13:00 +0700

    Create red_black_tree.h

---
+++ Fundamentals_of_Software_Design/C_language/Red_black_tree/red_black_tree.h
@@ -0,0 +1,44 @@
+#ifndef RED_BLACK_TREE_H
+#define RED_BLACK_TREE_H
+
+#include <stddef.h>
+
+typedef void * Pointer;
+
+typedef int (*CmpFunc)(Pointer data1, Pointer data2);
+
+typedef enum { RED, BLACK } Color;
+
+typedef struct tBSTNode {
+    Pointer data;
+    struct tBSTNode *left;
+    struct tBSTNode *right;
+    struct tBSTNode *parent;
+    Color color;
+} BSTNode;
+
+typedef struct tBST {
+    BSTNode *root;
+    CmpFunc cmp_func;
+    size_t size;
+} BST;
+
+BST * bst_create(CmpFunc cmp_func);
+
+void bst_clear(BST *tree);
+
+void bst_destroy(BST *tree);
+
+size_t bst_size(BST *tree);
+
+Pointer bst_find(BST *tree, Pointer data);
+
+Pointer bst_insert(BST *tree, Pointer data);
+
+Pointer bst_delete(BST *tree, Pointer data);
+
+void bst_foreach(BST *tree, void (*foreach_func)(Pointer data, Pointer extra_data), Pointer extra_data);
+
+int bst_check(BST *tree);
+
+#endif
