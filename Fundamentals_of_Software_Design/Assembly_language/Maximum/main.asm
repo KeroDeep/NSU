bd8c50fbd1c1ec1a88316ad9e6768af7b17be3c9
Klimov Bogdan <136115919+KeroDeep@users.noreply.github.com>
2025-09-04 12:54:52 +0700

    Update main.asm

--- Fundamentals_of_Software_Design/Assembly_language/Maximum/main.asm
+++ Fundamentals_of_Software_Design/Assembly_language/Maximum/main.asm
@@ -1,83 +1,83 @@
-section .data
-    prompt1 db 'Enter the number of elements (N): ', 0
-    prompt2 db 'Enter the numbers: ', 0
-    msg db 'Maximum: ', 0
+.data
+    prompt1: .asciz "Enter the number of elements (N): "
+    prompt2: .asciz "Enter the numbers: "
+    msg:     .asciz "Maximum: "
+    newline: .asciz "\n"
 
-section .bss
-    N resb 4                  ; переменная для хранения числа N
-    num resb 4                ; переменная для хранения каждого введенного числа
-    max resb 4                ; переменная для хранения максимума
+.bss
+    N:   .skip 4
+    num: .skip 4
+    max: .skip 4
 
-section .text
-    global _start
+.text
+    .globl _start
 
 _start:
-    ; Ввод количества чисел N
-    mov eax, 4                 ; syscall номер 4 (sys_write)
-    mov ebx, 1                 ; дескриптор файла 1 (stdout)
-    mov ecx, prompt1           ; адрес строки
-    mov edx, 30                ; длина строки
-    int 0x80                   ; системный вызов
+    li a7, 64
+    li a0, 1
+    la a1, prompt1
+    li a2, 30
+    ecall
 
-    mov eax, 3                 ; syscall номер 3 (sys_read)
-    mov ebx, 0                 ; дескриптор файла 0 (stdin)
-    mov ecx, N                 ; адрес для хранения N
-    mov edx, 4                 ; количество байт для чтения
-    int 0x80                   ; системный вызов
+    li a7, 63
+    li a0, 0
+    la a1, N
+    li a2, 4
+    ecall
 
-    ; Преобразуем N из строки в число
-    mov eax, [N]               ; загружаем N
-    sub eax, '0'               ; преобразуем из ASCII в число
-    mov ebx, eax               ; сохраняем количество чисел в ebx
+    lb t0, N
+    addi t0, t0, -48
+    mv t1, t0
 
-    ; Ввод чисел и нахождение максимума
-    mov eax, 0                 ; начинаем с нулевого максимума
-    mov [max], eax             ; сохраняем значение в переменную max
+    li t2, 0
+    sw t2, max
 
-.input_loop:
-    ; Выводим запрос на ввод числа
-    mov eax, 4                 ; syscall номер 4 (sys_write)
-    mov ebx, 1                 ; дескриптор файла 1 (stdout)
-    mov ecx, prompt2           ; адрес строки
-    mov edx, 15                ; длина строки
-    int 0x80                   ; системный вызов
+input_loop:
+    li a7, 64
+    li a0, 1
+    la a1, prompt2
+    li a2, 15
+    ecall
 
-    mov eax, 3                 ; syscall номер 3 (sys_read)
-    mov ebx, 0                 ; дескриптор файла 0 (stdin)
-    mov ecx, num               ; адрес для хранения введенного числа
-    mov edx, 4                 ; количество байт для чтения
-    int 0x80                   ; системный вызов
+    li a7, 63
+    li a0, 0
+    la a1, num
+    li a2, 4
+    ecall
 
-    ; Преобразуем введенное число в число
-    mov eax, [num]             ; загружаем число
-    sub eax, '0'               ; преобразуем из ASCII в число
+    lb t3, num
+    addi t3, t3, -48
 
-    ; Сравниваем с текущим максимумом
-    mov ebx, [max]             ; загружаем текущий максимум
-    cmp eax, ebx               ; сравниваем введенное число с максимумом
-    jle .next                   ; если текущее число меньше или равно, переходим к следующему
+    lw t4, max
+    ble t3, t4, skip_update
+    sw t3, max
 
-    ; Если текущее число больше, обновляем максимум
-    mov [max], eax             ; сохраняем новое максимальное значение
+skip_update:
+    addi t1, t1, -1
+    bnez t1, input_loop
 
-.next:
-    dec ebx                    ; уменьшаем счетчик чисел
-    jnz .input_loop            ; если не все числа введены, продолжаем вводить
+    li a7, 64
+    li a0, 1
+    la a1, msg
+    li a2, 9
+    ecall
 
-    ; Выводим результат
-    mov eax, 4                 ; syscall номер 4 (sys_write)
-    mov ebx, 1                 ; дескриптор файла 1 (stdout)
-    mov ecx, msg               ; адрес строки
-    mov edx, 9                 ; длина строки
-    int 0x80                   ; системный вызов
+    lw t5, max
+    addi t5, t5, 48
+    sb t5, max
 
-    mov eax, 4                 ; syscall номер 4 (sys_write)
-    mov ebx, 1                 ; дескриптор файла 1 (stdout)
-    mov ecx, max               ; адрес для вывода максимума
-    mov edx, 4                 ; количество байт для вывода
-    int 0x80                   ; системный вызов
+    li a7, 64
+    li a0, 1
+    la a1, max
+    li a2, 1
+    ecall
 
-    ; Завершаем программу
-    mov eax, 1                 ; syscall номер 1 (sys_exit)
-    xor ebx, ebx               ; код возврата 0
-    int 0x80                   ; системный вызов
+    li a7, 64
+    li a0, 1
+    la a1, newline
+    li a2, 1
+    ecall
+
+    li a7, 93
+    li a0, 0
+    ecall
