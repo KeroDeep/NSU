905f5d4bacd67fad843e7459815146024db0de1e
Klimov Bogdan <136115919+KeroDeep@users.noreply.github.com>
2025-09-04 12:58:42 +0700

    Update main.asm

--- Fundamentals_of_Software_Design/Assembly_language/Powers_of_two/main.asm
+++ Fundamentals_of_Software_Design/Assembly_language/Powers_of_two/main.asm
@@ -1,23 +1,14 @@
-section .text
-    global is_power_of_two  ; Экспортируем функцию
+.text
+    .globl is_power_of_two
 
 is_power_of_two:
-    ; Вход: eax = число N
-    ; Выход: eax = 1 (если степень двойки), 0 (иначе)
-    
-    test eax, eax       ; Проверяем, что N > 0
-    jz not_power        ; Если N == 0, сразу выход
-    
-    mov ecx, eax        ; Сохраняем N
-    dec ecx             ; ecx = N - 1
-    and eax, ecx        ; eax = N & (N - 1)
-    
-    jz is_power         ; Если eax == 0, число - степень двойки
-    
+    beqz a0, not_power
+    addi t0, a0, -1
+    and a0, a0, t0
+    beqz a0, is_power
 not_power:
-    xor eax, eax        ; eax = 0 (ложь)
+    li a0, 0
     ret
-    
 is_power:
-    mov eax, 1          ; eax = 1 (истина)
+    li a0, 1
     ret
